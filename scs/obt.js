2f7d4d3458e2fd4ffc313081a61ba4d165138887
kipelia <eliakimkiplagat441@gmail.com>
2025-02-18 16:38:09 +0300

    Update obt.js

--- scs/obt.js
+++ scs/obt.js
@@ -1,7 +1,7 @@
 const JavaScriptObfuscator = require("javascript-obfuscator");
 const {
   adams
-} = require("../Ibrahim/adams");
+} = require("../Eliakim/tech");
 adams({
   'nomCom': "obt",
   'categorie': 'General'
